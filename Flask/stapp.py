e311245c1ee437e3958e2a42e2686d6ef7c9f0c8
mzhub <mishabhadhi@gmail.com>
2025-02-14 05:45:23 +0000

    push

--- Flask/stapp.py
+++ Flask/stapp.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title("Hello World!, This is a Stramlit ap")
 if st.button("Click here"):
     st.write("Yey it works.....")
+
+    if st.button("Click here again"):
+    st.write("Change")
+
