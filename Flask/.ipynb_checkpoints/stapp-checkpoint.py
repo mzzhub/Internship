b1c0a726df76ed85ae659eb299b221f9ebc1e82f
mzhub <mishabhadhi@gmail.com>
2025-02-14 05:52:56 +0000

    push

---
+++ Flask/.ipynb_checkpoints/stapp-checkpoint.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello World!, This is a Stramlit ap")
+if st.button("Click here"):
+    st.write("Yey it works.....")
+
+    if st.button("Click here again"):
+    st.write("Change")
+
